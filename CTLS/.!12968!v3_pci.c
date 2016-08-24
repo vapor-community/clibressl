131b05c0284951f2131647b81eb7ee448e0a3f2b
Tanner Nelson <me@tanner.xyz>
2016-08-24 15:17:44 -0400

    remove weird files

--- CTLS/.!12968!v3_pci.c
+++
@@ -1,4 +0,0 @@
-/* $OpenBSD: v3_pci.c,v 1.9 2015/07/19 01:20:32 doug Exp $ */
-/* Contributed to the OpenSSL Project 2004
- * by Richard Levitte (richard@levitte.org)
- */
--- CTLS/.!13032!v3_pcia.c
+++
@@ -1,4 +0,0 @@
-/* $OpenBSD: v3_pcia.c,v 1.5 2015/02/09 16:03:11 jsing Exp $ */
-/* Contributed to the OpenSSL Project 2004
- * by Richard Levitte (richard@levitte.org)
- */
