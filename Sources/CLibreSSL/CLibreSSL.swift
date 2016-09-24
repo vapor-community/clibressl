60732b3eb60f237825554c26de768f66d8201bd1
Tanner Nelson <me@tanner.xyz>
2016-09-24 01:14:06 -0400

    linux vs mac

--- Sources/CLibreSSL/CLibreSSL.swift
+++ Sources/CLibreSSL/CLibreSSL.swift
@@ -1,7 +1,9 @@
 #if os(Linux)
 	@_exported import ClibcryptoLinux
+	@_exported import ClibtlsLinux
 #else
 	@_exported import ClibcryptoMac
+	@_exported import ClibtlsMac
 #endif
-@_exported import Clibtls
+
 
