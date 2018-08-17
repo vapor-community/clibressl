24248d496da6613ddeb0b0954a461f3ade3d155b
Jared Sorge <jsorge@lyft.com>
2018-08-17 07:44:07 -0700

    Restore the old package description, add v4 as separate file

--- Package.swift
+++ Package.swift
@@ -1,12 +1,3 @@
-// swift-tools-version:4.1
 import PackageDescription
 
-let package = Package(
-    name: "CLibreSSL",
-    products: [
-    	.library(name: "CLibreSSL", targets: ["CLibreSSL"]),
-    ],
-    targets: [
-    	.target(name: "CLibreSSL", dependencies: [])
-    ]
-)
+let package = Package(name: "CLibreSSL")
